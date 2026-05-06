3a7a49aa80a1651e9d02b80b0191c712432ce799
kimtaewon6670 <20222022@edu.hanbat.ac.krS>
2026-05-06 15:52:39 +0900

    feat: 메인화면 컴포넌트 및 MVC 구조 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import ComplaintController from "./controllers/ComplaintController";
 
 function App() {
   return (
-    <div>
-      <ComplaintController />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ComplaintController />} />
+        <Route path="/complaints" element={<ComplaintController />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
